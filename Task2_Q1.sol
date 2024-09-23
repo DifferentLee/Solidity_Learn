6cfdd2d1676edbbe9d5e6e49f8c4b9283973b0ec
DifferentLee <liyiraner@163.com>
2024-09-23 11:29:26 +0800

    Update Task2_Q1.sol

--- Task2_Q1.sol
+++ Task2_Q1.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
 contract InsertSort {
     function insertSort (uint[] memory a) public pure returns(uint[] memory) {
         for (uint i = 1; i < a.length; i++) {
             uint j = i;
             uint key = a[i];
-            while ((a[j - 1] > key) && (j >= 1)) {
+            while ((j >= 1) && (a[j - 1] > key)) {
                 a[j] = a[j - 1];
                 j--;
             } 
